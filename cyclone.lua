fe4c07f43e5412e137f7b8e70a981b306e570b48
badgeminer <64038298+badgeminer@users.noreply.github.com>
2024-05-26 20:32:28 -0600

    Update cyclone.lua

--- cyclone.lua
+++ cyclone.lua
@@ -1,13 +1,25 @@
+package.path = package.path .. ";/usr/lib/?;/usr/lib/?.lua;/usr/lib/?/init.lua"
 local toml = require("toml")
-local f = fs.open("/etc/repo.toml","r")
-local repos = toml.decode(f.readAll())
+local f = fs.open("/etc/repo.ltn","r")
+local repos = textutils.unserialize(f.readAll())
 f.close()
 
 local args = {...}
 local cmd = args[1]
+local function getPkgData(pki)
+    local f = fs.open("/var/cyclone/pkgs.ltn","r")
+    local pkgs = textutils.unserialize(f.readAll())
+    f.close()
+    local pk = pkgs[pki]
+    if pk.SIMLNK then
+        pk = pkgs[pk.SIMLNK]
+    end
+    return pk
+end
 local function sync()
     io.stdout:write("syncing package lists\n")
     local pkgsD = {}
+    local pkgsL = {}
     local reposUsed = {}
     for key, value in pairs(repos) do
         if reposUsed[key] then
@@ -16,6 +28,7 @@ local function sync()
             io.stdout:write("fetching repo "..key.."\n")
             local r = http.get(value.url.."packs.toml")
             local pk = toml.decode(r.readAll())
+            --print(textutils.serialise(pk))
             for k, v in pairs(pk) do
                 if v.location == "local" then
                     v.url = value.url..k..".toml"
@@ -23,10 +36,16 @@ local function sync()
                 io.stdout:write("fetching "..key.."."..k.."\n")
                 local R = http.get(v.url)
                 local pak = toml.decode(R.readAll())
-                if pkgsD[k] then
+                pak.package.nmx = k
+                if pkgsL[k] then
                     pkgsD[key.."."..k] = pak.package
+                    pkgsL[key.."."..k] = true
+                    
                 else
-                    pkgsD[k] = pak.package
+                    pkgsD[k] = {SIMLNK=key.."."..k}
+                    pkgsD[key.."."..k] = pak.package
+                    pkgsL[k] = true
+                    pkgsL[key.."."..k] = true
                 end
             end
         end
@@ -37,31 +56,39 @@ local function sync()
     f.close()
 end
 local function updateIns(k,v)
-    local f = fs.open("/var/cyclone/installed.toml","r")
-    local pkgs = toml.decode(f.readAll())
+    local f = fs.open("/var/cyclone/installed.ltn","r")
+    local pkgs = textutils.unserialize(f.readAll())
     f.close()
     pkgs[k] = v
-    f = fs.open("/var/cyclone/installed.toml","w")
-    f.write(toml.encode(pkgs))
+    f = fs.open("/var/cyclone/installed.ltn","w")
+    f.write(textutils.serialise(pkgs))
     f.close()
 end
 local function isIns(k)
-    local f = fs.open("/var/cyclone/installed.toml","r")
-    local pkgs = toml.decode(f.readAll())
+    local f = fs.open("/var/cyclone/installed.ltn","r")
+    local pkgs = textutils.unserialize(f.readAll())
     f.close()
     return pkgs[k]
 end
 local function add()
-    local f = fs.open("/etc/repo.toml","w")
+    local f = fs.open("/etc/repo.ltn","w")
     repos[args[2]] = {url=args[3]}
-    f.write(toml.encode(repos))
+    f.write(textutils.serialise(repos))
     f.close()
 end
+local function pkgPath(pk)
+    local pki = pk.nmx
+    if pk.fname then
+        pki = pk.fname
+    end
+    if pk.loc then
+        return pk.loc.."/"..pki..".lua"
+    else
+        return "/usr/"..pk.type.."/"..pki..".lua"
+    end
+end
 local function install(pki)
-    local f = fs.open("/var/cyclone/pkgs.ltn","r")
-    local pkgs = textutils.unserialize(f.readAll())
-    f.close()
-    local pk = pkgs[pki]
+    local pk = getPkgData(pki)
     if isIns(pki) then
         io.stdout:write(pki.." ["..isIns(pki).."]".." is installed\n")
     elseif pk then
@@ -72,7 +99,7 @@ local function install(pki)
             end
         end
         io.stdout:write("installing "..pki.."\n")
-        local fa = fs.open("/usr/"..pk.type.."/"..pki..".lua","w")
+        local fa = fs.open(pkgPath(pk),"w")
         local R = http.get(pk.addr)
         fa.write(R.readAll())
         fa.close()
@@ -85,7 +112,7 @@ local function update(pki)
     local f = fs.open("/var/cyclone/pkgs.ltn","r")
     local pkgs = textutils.unserialize(f.readAll())
     f.close()
-    local pk = pkgs[pki]
+    local pk = getPkgData(pki)
     if pk then
     else
         return
@@ -100,7 +127,7 @@ local function update(pki)
             end
         end
         io.stdout:write("updating "..pki.."\n")
-        local fa = fs.open("/usr/"..pk.type.."/"..pki..".lua","w")
+        local fa = fs.open(pkgPath(pk),"w")
         local R = http.get(pk.addr)
         fa.write(R.readAll())
         fa.close()
@@ -108,8 +135,8 @@ local function update(pki)
     end
 end
 local function updateAll()
-    local f = fs.open("/var/cyclone/installed.toml","r")
-    local pkgsI = toml.decode(f.readAll())
+    local f = fs.open("/var/cyclone/installed.ltn","r")
+    local pkgsI = textutils.unserialize(f.readAll())
     f.close()
     for key, value in pairs(pkgsI) do
         update(key)
